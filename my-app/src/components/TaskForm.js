ed20b5d94e5c7617ff0c18686feed9fdcaae40b5
kapitaaaana <89232439+kapitaaaana@users.noreply.github.com>
2023-12-15 02:41:57 +0000

    2

--- my-app/src/components/TaskForm.js
+++ my-app/src/components/TaskForm.js
@@ -1,56 +1,5 @@
+
 import { useState } from "react";
-import "./taskform.css";
-
-export default function TaskForm({ handleAdd }) {
-  const [task, setTask] = useState("");
-  const [category, setCategory] = useState("others");
-  const [dueDate, setDueDate] = useState("");
-
-  const addTask = () => {
-    handleAdd(task, category, dueDate, false);
-    setTask("");
-    setCategory("others");
-    setDueDate("");
-  };
-
-  return (
-    <div className="tform">
-      <h3>Add Task</h3>
-      <input
-        type="text"
-        name="task"
-        id="task"
-        placeholder="Enter Task"
-        onChange={(e) => setTask(e.target.value)}
-        value={task}
-      />
-      <select
-        name="category"
-        id="category"
-        value={category}
-        onChange={(e) => setCategory(e.target.value)}
-      >
-        <option value="work">School</option>
-        <option value="house">House</option>
-        <option value="others">Others</option>
-      </select>
-      <input
-        type="date"
-        name="dueDate"
-        id="dueDate"
-        value={dueDate}
-        onChange={(e) => setDueDate(e.target.value)}
-      />
-      <div className="check">
-        <input type="checkbox" name="prio" id="prio" />
-        <label htmlFor="prio">Priority</label>
-      </div>
-      <button onClick={addTask}>Add</button>
-    </div>
-  );
-}
-
-/*import { useState } from "react";
 import "./taskform.css";
 
 export default function TaskForm({ handleAdd }) {
@@ -89,4 +38,4 @@ export default function TaskForm({ handleAdd }) {
       <button onClick={addTask}>Add</button>
     </div>
   );
-}*/
+}
